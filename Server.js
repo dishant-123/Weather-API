e367e78a70a7ae9e49315d22479b5e15356af7e7
unknown <dishantdua8956@gmail.com>
2019-12-15 18:30:29 +0530

    add Server.js

---
+++ Server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+var bodyParser = require('body-parser');
+const path = require('path');
+require('dotenv').config()
+//middle ware
+app.use(cors());
+ app.use(bodyParser.json()); 
+ 
+app.use(express.static(path.join(__dirname,'frontend','build')));
+
+ app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'frontend','build','index.html'));
+})
+
+
+const PORT = process.env.PORT || 3007
+app.listen(PORT, () =>{
+    console.log('Connected');
+})
+
